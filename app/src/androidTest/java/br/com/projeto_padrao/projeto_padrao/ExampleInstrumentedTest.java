99b5010898cc253a45b4efd4eb8924f32786873a
Italo Alan <italoalanw3@gmail.com>
2016-11-06 22:37:50 -0300

    Criado testes para verificar permissão do sistema android para requisicao; Teste para testar API (conexão, status, retorno);

--- app/src/androidTest/java/br/com/projeto_padrao/projeto_padrao/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/br/com/projeto_padrao/projeto_padrao/ExampleInstrumentedTest.java
@@ -7,7 +7,21 @@ import android.support.test.runner.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import java.io.IOException;
+import java.net.ConnectException;
+import java.util.List;
+
+import br.com.projeto_padrao.projeto_padrao.api.API;
+import br.com.projeto_padrao.projeto_padrao.api.url.BolaoService;
+import br.com.projeto_padrao.projeto_padrao.modelo.Bolao;
+import retrofit2.Call;
+import retrofit2.GsonConverterFactory;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
 
 /**
  * Instrumentation test, which will execute on an Android device.
@@ -22,5 +36,33 @@ public class ExampleInstrumentedTest {
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("br.com.projeto_padrao.projeto_padrao", appContext.getPackageName());
+    }
+
+    @Test
+    public void testar_request_get() {
+        Retrofit retrofit = new Retrofit.Builder().baseUrl(API.URL).addConverterFactory(GsonConverterFactory.create()).build();
+
+        BolaoService bolaoService = retrofit.create(BolaoService.class);
+
+        assertNotNull(bolaoService);
+
+        Call<List<Bolao>> fila = bolaoService.listBoloesAtivos();
+
+        try {
+            Response<List<Bolao>> execute = fila.execute();
+            assertEquals(execute.code(), 200);
+            assertNotNull(execute.body());
+        } catch (ConnectException e) {
+            if (e.getMessage().contains("connect to localhost")) {
+                fail("Conexão com API não deve usar LOCALHOST");
+            } else {
+                fail("Conexão com API falhou: " + e.getMessage());
+            }
+        } catch (IOException e) {
+            if (e.getMessage().toLowerCase().contains("failed to connect"))
+                fail("Falha ao conectar na API: " + e.getMessage());
+            else
+                fail("Erro não tratado para o teste: " + e.getMessage());
+        }
     }
 }
